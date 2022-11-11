3c81960ecc7bfa040b793697ea6d882ecdc67963
Wilkar Márquez <wilkarmarquez13@gmail.com>
2022-11-10 19:38:57 -0500

    prueba railway

--- src/app/private/modules/peticiones/peticiones.component.ts
+++ src/app/private/modules/peticiones/peticiones.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CalendarOptions, defineFullCalendarElement, FullCalendarElement } from '@fullcalendar/web-component';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import esLocale  from '@fullcalendar/core/locales/es';
+import { AuthService } from 'src/app/service/autentication/auth.service';
+import { userLogin } from 'src/app/models/userLogin';
+import interactionPlugin from '@fullcalendar/interaction';
+
+// defineFullCalendarElement(peticiones);
 
 @Component({
   selector: 'app-peticiones',
@@ -7,9 +16,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeticionesComponent implements OnInit {
 
-  constructor() { }
+  calendarOptions: CalendarOptions = Object.assign({},FullcalendarC.optionsCalendar);
+  @ViewChild('peticiones') peticiones:FullCalendarElement | undefined;
+  user: userLogin | null;
+  mostrarInfo: boolean;
+  infoFechaSeleccionada: {};
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.user = { id:-1,firstName:'',lastName:'',email:'',role:-1,token:'' };
+    this.mostrarInfo =  false;
+    this.infoFechaSeleccionada = Date();
   }
 
+  ngOnInit(): void {
+    this.user = this.authService.getDatosAutenticacion();
+    this.motrarSeleccionarTurno();
+  }
+
+  motrarSeleccionarTurno():void{
+    this.calendarOptions.dateClick = (info) => {
+      this.mostrarInfo = true;
+      this.infoFechaSeleccionada = info.date.getDay;
+      
+    }
+  }
+
+}
+  
+export class FullcalendarC {
+
+  public static optionsCalendar: CalendarOptions = {
+    locale:esLocale,
+    weekends: false,
+    slotDuration:{minutes:15},
+    slotLabelFormat: { hour: 'numeric', minute: '2-digit', omitZeroMinute: false, hour12: false, meridiem: 'short'},
+    dayHeaderFormat: {weekday: 'short', month: 'numeric', day: 'numeric', omitCommas: true},
+    nowIndicator: true,
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+    },
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+    initialView: 'timeGridWeek',
+    slotMinTime: "08:00:00",
+    slotMaxTime: "18:00:00",
+    height: '90%'
+  };
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  urlRoles:      'http://localhost:8000/api/roles/',
+  urlPlaces:     'http://localhost:8000/api/places/',
+  urlEntities:   'http://localhost:8000/api/entities/',
+  urlTurns:      'http://localhost:8000/api/turns/',
+  urlTurnStatus: 'http://localhost:8000/api/turnstatus/',
+  urlUsers:      'http://localhost:8000/api/users/'
 };
